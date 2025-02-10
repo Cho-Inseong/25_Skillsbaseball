6d7d880446b1a7d344774739048e2091ff58c553
choinseong <choinseong2237@gmail.com>
2025-02-10 17:56:15 +0900

    헤더, 푸터 컴포넌트화화

---
+++ component/footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <img class="logo" src="./img/logo.png" alt="">
+    <p>
+        [Copyright]<br>
+        Copyrightⓒ Skills baseball park. All rights reserved.<br>
+        Call: 142-3677<br>
+        Email: help@skillsbaseballpark.com<br>
+        Address: (12345) 서울특별서 중구 중북로 71<br>
+    </p>
+</footer>
---
+++ component/header.php
@@ -0,0 +1,15 @@
+<header>
+    <div>
+        <img class="logo" src="./img/logo.png" alt="">
+        <ul>
+            <li><a title="Information" href="./page/sub01.html">Information</a></li>
+            <li><a title="Statistics" href="./page/sub02.html">Statistics</a></li>
+            <li><a title="Reservation" href="#">Reservation</a></li>
+            <li><a title="Goods" href="#">Goods</a></li>
+        </ul>
+    </div>
+    <ul class="sing">
+        <li><a title="sign_in" href="#">sign_in</a></li>
+        <li><a title="sign_up" href="#">sign_up</a></li>
+    </ul>
+</header>
